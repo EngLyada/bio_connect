794c66ca5a313329d28ff1d92eaa93c4a4da3777
Lyada Emmanuel <lyadaemmanuel@gmail.com>
2022-04-16 13:13:37 +0300

    bootstrap

--- resources/views/layout/man-layout.blade.php
+++ resources/views/layout/man-layout.blade.php
@@ -5,18 +5,26 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BIO-Connect- @yield('title')</title>
+    <link rel="stylesheet" href="bootstrap.css">
 </head>
-<body>
-  <h1>  @yield('heading')</h1>
+<body class="container">
+  <div class="row mt-5">
+    <div class="col-md-4">
+      <div class="list-group">
+      <a class="list-group-item" >MAIN MENU</a>
+        <a class="list-group-item" href='/'>Home</a>
+        <a class="list-group-item" href='/about'>About Us</a>
+        <a class="list-group-item" href='/contact'>Contact Us</a>
+      </div>
+    </div>
+    <div class="col-md-8 border-left">
+        <h1>  @yield('heading')</h1>
+        @yield('content')
+    </div>
+  </div>
+  
+    
     <hr>
-    <ul>
-      <li ><a href='/'>Home</a></li>
-      <li><a href='/about'>About Us</a></li>
-      <li><a href='/contact'>Contact Us</a></li>
-    </ul>
-    @yield('content')
-
-    <hr>
-    <b>(C) <?php echo date('Y'); ?> BIO-Connect. All rights reserved by LYSMU LIMITED </b>
+    <div align="center"><b>(C) <?php echo date('Y'); ?> BIO-Connect. All rights reserved by LYSMU LIMITED </b></p>
 </body>
 </html>
